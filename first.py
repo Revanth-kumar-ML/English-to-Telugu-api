c8e124dbd4a93dfbe02c01c3506b6601b40e0416
C Revanth kumar <cgangadhar214@gmail.com>
2026-02-25 09:48:25 +0530

    Added frontend route, another language and improved API validation

--- first.py
+++ first.py
@@ -1,28 +1,56 @@
 from transformers import AutoModelForSeq2SeqLM, AutoTokenizer  # pip install transformers and pip install torch
-from flask import Flask, request, jsonify  # pip install flask
+from flask import Flask, request, jsonify, render_template  # pip install flask
 
 application = Flask(__name__)
 
-# Load the model and tokenizer
-model_name = "Meher2006/english-to-telugu-model"
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+# English to Telugu Load the model and tokenizer
+eng_to_tel_model_name = "Meher2006/english-to-telugu-model"
+eng_to_tel_tokenizer = AutoTokenizer.from_pretrained(eng_to_tel_model_name)
+eng_to_tel_model = AutoModelForSeq2SeqLM.from_pretrained(eng_to_tel_model_name)
 
-# Function to translate text
-def translate(text):
-    inputs = tokenizer(text, return_tensors="pt")
-    outputs = model.generate(**inputs)
-    return tokenizer.decode(outputs[0], skip_special_tokens=True)
+# English to Hindi Loding
+eng_to_hin_model_name = "Helsinki-NLP/opus-mt-en-hi"
+eng_to_hin_tokenizer = AutoTokenizer.from_pretrained(eng_to_hin_model_name)
+eng_to_hin_model = AutoModelForSeq2SeqLM.from_pretrained(eng_to_hin_model_name)
 
-@application.route("/user-text-translate", methods=["POST"])
-def translation_api():
+# Function to translate text from English to Hindi
+def translate_eng_to_hin(text):
+    inputs = eng_to_hin_tokenizer(text, return_tensors="pt")
+    outputs = eng_to_hin_model.generate(**inputs)
+    return eng_to_hin_tokenizer.decode(outputs[0], skip_special_tokens=True)
+
+# Function to translate text from English to Telugu
+def translate_eng_to_tel(text):
+    inputs = eng_to_tel_tokenizer(text, return_tensors="pt")
+    outputs = eng_to_tel_model.generate(**inputs)
+    return eng_to_tel_tokenizer.decode(outputs[0], skip_special_tokens=True)
+
+@application.route("/english-to-telugu", methods=["POST"])
+def english_to_telugu_api():
     sentence = request.json["sentence"]
-    translated_text = translate(sentence)
+    translated_text = translate_eng_to_tel(sentence)
     response_json = {
         "original_text": sentence,
         "translated_text": translated_text
     }
     return jsonify(response_json)
+
+@application.route("/english-to-hindi", methods=["POST"])
+def english_to_hindi_api():
+    sentence = request.json["sentence"]
+    translated_text = translate_eng_to_hin(sentence)
+    print(translated_text + "hindi")
+    response_json = {
+        "original_text": sentence,
+        "translated_text": translated_text
+    }
+    return jsonify(response_json)
+
+
+@application.route("/")
+def home():
+    return render_template("index.html")
+
 
 if __name__ == "__main__":
     application.run()
